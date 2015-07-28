1c5c3fb2a997bce07bdc2ab67f7d7d0f2fb82f15
Freddy Rodriguez <cristinamar2000@gmail.com>
2015-07-28 18:44:47 -0430

    ver contactos

--- js/contact.js
+++ js/contact.js
@@ -26,6 +26,43 @@ function ContactView(){
       $("[softphone] .search_panel input[type='text']").val("");
 
   }
+
+  this.showContacts = function(){
+    $("[softphone] .numbers_panel").hide();
+    var contactPanel = $("[softphone] .panel");
+    contactPanel.children().remove();
+    callView.cleanMessage();
+    contactPanel.show();
+
+    var contacts =  JSON.parse(window.localStorage.contacts);
+    contactPanel.append("<div class='contacts_fix_container'></div>");
+    contactPanel.children(".contacts_fix_container")
+      .append("<div class='contacts_container'></div>");
+
+    var contactsContainer = contactPanel.find(".contacts_fix_container .contacts_container");
+
+    if (contacts.length > 0){
+      for (var i = 0; i < contacts.length; i++){
+        contactsContainer.append(
+          "<div class='contact_row'>" +
+            "<div class='contact_field'>" +
+              "<img src='img/hombre.png'>" +
+              "<span>" + contacts[i].name + "</span>" +
+            "</div>" +
+
+            "<div class='contact_field'>" +
+              "<img src='img/telefono.png'/>" +
+              "<span>" + contacts[i].number + "</span>" +
+            "</div>" +
+
+          "</div>"
+        );
+
+      }
+    }else{
+      softphoneView.showErrorMessage("No contacts to show");
+    }
+  }
 }
 
 var contactView = new ContactView();
