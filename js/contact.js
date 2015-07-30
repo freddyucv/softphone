4a22efd3d59f2348cc2289b9b3414ec39a761985
Freddy Rodriguez <cristinamar2000@gmail.com>
2015-07-29 23:59:52 -0430

    correcion de errores

--- js/contact.js
+++ js/contact.js
@@ -1,6 +1,7 @@
 function ContactView(){
 
   this.newContact = function(){
+
       $("[softphone] .numbers_panel").hide();
       var contactPanel = $("[softphone] .panel");
       contactPanel.show();
@@ -53,15 +54,14 @@ function ContactView(){
     callView.cleanMessage();
     contactPanel.show();
 
-    var contacts =  JSON.parse(window.localStorage.contacts);
-    contactPanel.append("<div class='contacts_fix_container'></div>");
-    contactPanel.children(".contacts_fix_container")
-      .append("<div class='contacts_container'></div>");
+    if (window.localStorage.contacts){
+      var contacts =  JSON.parse(window.localStorage.contacts);
+      contactPanel.append("<div class='contacts_fix_container'></div>");
+      contactPanel.children(".contacts_fix_container")
+        .append("<div class='contacts_container'></div>");
 
-    var contactsContainer = contactPanel.find(".contacts_fix_container .contacts_container");
+      var contactsContainer = contactPanel.find(".contacts_fix_container .contacts_container");
 
-
-    if (contacts.length > 0){
       contactsContainer.append(this.getContactsToShow(contacts, true, false));
     }else{
       callView.showErrorMessage("No contacts to show");
@@ -99,39 +99,43 @@ function ContactView(){
 
   this.searchContacts = function(){
 
-      var search = $("#phoneNumber").val();
-      var contacts =  JSON.parse(window.localStorage.contacts);
+      if (window.localStorage.contacts){
+        var search = $("#phoneNumber").val();
+        var contacts =  JSON.parse(window.localStorage.contacts);
 
-      var filter = contacts.filter(function(item){
-                      var itemNames = item.name.split(" ");
-                      var result = false;
+        var filter = contacts.filter(function(item){
+                        var itemNames = item.name.split(" ");
+                        var result = false;
 
-                      for(var i = 0; i < itemNames.length; i++){
-                          if (itemNames[i].indexOf(search) == 0){
-                            return true;
-                          }
-                      }
+                        for(var i = 0; i < itemNames.length; i++){
+                            if (itemNames[i].indexOf(search) == 0){
+                              return true;
+                            }
+                        }
 
-                    });
+                      });
 
-      $("#search_result").children().remove();
-      $("#search_result").append(this.getContactsToShow(filter, false, true));
+        $("#search_result").children().remove();
+        $("#search_result").append(this.getContactsToShow(filter, false, true));
 
-      if (filter.length > 0){
-        $("#search_result_content").show();
-        $("[softphone] .numbers_panel").hide();
+        if (filter.length > 0){
+          $("#search_result_content").show();
+          $("[softphone] .numbers_panel").hide();
+        }
       }
   }
 
   this.hideSearchContactsPanel = function(showNumberPanel, cleanText){
-    $("#search_result_content").hide();
+    if ($("#search_result_content").is(":visible")){
+      $("#search_result_content").hide();
 
-    if (cleanText){
-      $("[softphone] .search_panel input[type='text']").val("");
-    }
+      if (cleanText){
+        $("[softphone] .search_panel input[type='text']").val("");
+      }
 
-    if (showNumberPanel){
-      $("[softphone] .numbers_panel").show();
+      if (showNumberPanel){
+        $("[softphone] .numbers_panel").show();
+      }
     }
   }
 }
